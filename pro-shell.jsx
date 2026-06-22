512364d3cd30def018b477bda96b5d5d7f059577
Grzegorz Piskalski <g.piskalski@gmail.com>
2026-06-22 20:25:42 +0000

    Dodaj Politykę Cookies jako statyczną stronę, podlinkować w stopce

--- pro-shell.jsx
+++ pro-shell.jsx
@@ -376,7 +376,8 @@ const ProFooter = ({ onNav }) => (
         <div>
           <div style={footColHead}>Prawne</div>
           <a key="prywatnosc" href="/polityka-prywatnosci.html" style={footLink}>Polityka prywatności</a>
-          {[['ustawienia', 'Ustawienia prywatności'], ['cookies', 'Polityka cookies']].map(([id, l]) => (
+          <a key="cookies" href="/polityka-cookies.html" style={footLink}>Polityka cookies</a>
+          {[['ustawienia', 'Ustawienia prywatności']].map(([id, l]) => (
             <button key={id} onClick={() => onNav({ type: 'page', id })} style={footLink}>{l}</button>
           ))}
         </div>
